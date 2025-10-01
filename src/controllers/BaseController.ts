30e87465122ffeb9a1e022e461dea867fa111222
Albert Akrong <alphaxsalt@gmail.com>
2025-10-01 08:54:52 +0100

    remove binding in base controller

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -2,17 +2,6 @@ import { PageName } from '../config/pages';
 import { InertiaAdapter } from '../adapters/InertiaHonoAdapter';
 
 export class BaseController {
-	constructor() {
-		// Auto-bind all methods to preserve 'this' context
-		const methodNames = Object.getOwnPropertyNames(Object.getPrototypeOf(this)).filter(
-			(name) => name !== 'constructor' && typeof this[name as keyof this] === 'function'
-		);
-
-		methodNames.forEach((name) => {
-			this[name as keyof this] = (this[name as keyof this] as Function).bind(this);
-		});
-	}
-
 	public async render(c: any, componentName: PageName, componentProps: any = {}, documentMetadata: any = {}) {
 		const inertia = new InertiaAdapter({
 			version: '1',
@@ -20,10 +9,9 @@ export class BaseController {
 		const result = inertia.render(c, componentName, componentProps);
 
 		if (result instanceof Response) {
-			return result; // Inertia XHR request
+			return result;
 		}
 
-		// First page load - load template and render HTML
 		const templateResponse = await c.env.ASSETS.fetch(new URL('/template.html', c.req.url));
 		if (!templateResponse.ok) {
 			throw new Error('Template not found');
